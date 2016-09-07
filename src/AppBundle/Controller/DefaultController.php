bb7f72d6f9bef3b709dba8283cad5e62d4b30773
Robert M. Parker <rparker@yamiko.ninja>
2016-09-07 03:00:55 -0700

    initial dummy content

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,10 +4,37 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Response;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render(
+            'default/login.html.twig',
+            array(
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            )
+        );
+    }
+
+    /**
+     * @Route("/admin", name="dashboard")
+     */
+    public function adminAction(Request $request)
+    {
+        return new \Symfony\Component\HttpFoundation\Response("<h1>Hello Admin</h1>");
+    }
+
     /**
      * @Route("/", name="homepage")
      */
